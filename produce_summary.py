0a2a64efbbbb97db2f15f6ce8cbb858ba15883b7
Dominique Cuevas <cuevasdominique@gmail.com>
2019-10-03 05:17:48 +0000

    Define function to passthrough multiple days and test on Day 1

--- produce_summary.py
+++ produce_summary.py
@@ -1,43 +1,61 @@
-print("Day 1")
-the_file = open("um-deliveries-20140519.txt") # open 5/19 daily text data file to use in python
-for line in the_file: # iterate each line in 5/19 file
-    line = line.rstrip() # strips right whitespaces
-    words = line.split('|') # turn string into list at every '|'
+# print("Day 1")
+# the_file = open("um-deliveries-20140519.txt") # open 5/19 daily text data file to use in python
+# for line in the_file: # iterate each line in 5/19 file
+#     line = line.rstrip() # strips right whitespaces
+#     words = line.split('|') # turn string into list at every '|'
 
-    melon = words[0] 
-    count = words[1] # changed 0 index to 1
-    amount = words[2] # changed 0 index to 2
+#     melon = words[0] 
+#     count = words[1] # changed 0 index to 1
+#     amount = words[2] # changed 0 index to 2
 
-    print("Delivered {} {}s for total of ${}".format(
-        count, melon, amount))
-the_file.close()
-
-
-print("Day 2")
-the_file = open("um-deliveries-20140520.txt")
-for line in the_file:
-    line = line.rstrip()
-    words = line.split('|')
-
-    melon = words[0]
-    count = words[0]
-    amount = words[0]
-
-    print("Delivered {} {}s for total of ${}".format(
-        count, melon, amount))
-the_file.close()
+#     print("Delivered {} {}s for total of ${}".format(
+#         count, melon, amount))
+# the_file.close()
 
 
-print("Day 3")
-the_file = open("um-deliveries-20140521.txt")
-for line in the_file:
-    line = line.rstrip()
-    words = line.split('|')
+# print("Day 2")
+# the_file = open("um-deliveries-20140520.txt")
+# for line in the_file:
+#     line = line.rstrip()
+#     words = line.split('|')
 
-    melon = words[0]
-    count = words[0]
-    amount = words[0]
+#     melon = words[0]
+#     count = words[0]
+#     amount = words[0]
 
-    print("Delivered {} {}s for total of ${}".format(
-        count, melon, amount))
-the_file.close()
+#     print("Delivered {} {}s for total of ${}".format(
+#         count, melon, amount))
+# the_file.close()
+
+
+# print("Day 3")
+# the_file = open("um-deliveries-20140521.txt")
+# for line in the_file:
+#     line = line.rstrip()
+#     words = line.split('|')
+
+#     melon = words[0]
+#     count = words[0]
+#     amount = words[0]
+
+#     print("Delivered {} {}s for total of ${}".format(
+#         count, melon, amount))
+# the_file.close()
+
+
+def print_delivery_report(day, daily_file):
+    print("Day", day)
+    the_file = open(daily_file) # open daily text data file to use in python
+    for line in the_file: # iterate each line in 5/19 file
+        line = line.rstrip() # strips right whitespaces
+        words = line.split('|') # turn string into list at every '|'
+
+        melon = words[0] 
+        count = words[1] # changed 0 index to 1
+        amount = words[2] # changed 0 index to 2
+
+        print("Delivered {} {}s for total of ${}".format(
+            count, melon, amount))
+    the_file.close()
+
+print_delivery_report(1, "um-deliveries-20140519.txt")
