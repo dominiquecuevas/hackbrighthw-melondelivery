29774e982cd3dc01cfb4b18c7f0e591675184c7c
Dominique Cuevas <cuevasdominique@gmail.com>
2019-10-03 05:06:34 +0000

    Debugged Day 1 indexes

--- produce_summary.py
+++ produce_summary.py
@@ -1,12 +1,12 @@
 print("Day 1")
-the_file = open("um-deliveries-20140519.txt")
-for line in the_file:
-    line = line.rstrip()
-    words = line.split('|')
+the_file = open("um-deliveries-20140519.txt") # open 5/19 daily text data file to use in python
+for line in the_file: # iterate each line in 5/19 file
+    line = line.rstrip() # strips right whitespaces
+    words = line.split('|') # turn string into list at every '|'
 
-    melon = words[0]
-    count = words[0]
-    amount = words[0]
+    melon = words[0] 
+    count = words[1] # changed 0 index to 1
+    amount = words[2] # changed 0 index to 2
 
     print("Delivered {} {}s for total of ${}".format(
         count, melon, amount))
